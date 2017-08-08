c5fd38b38a4e3f9c9a60f1f7c37dce3c28b560d9
nestattacked <yang283349242@gmail.com>
2017-08-08 22:32:31 +0800

    add more words into databases && add column definition

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ router.get('/regdict/v1/words', async function(ctx) {
   let offset = Number(ctx.query.offset)||0;
   let limit = Number(ctx.query.limit)||20;
   
-  let words = await pool.query('select word,us_audio,us_pron,has_audio from words where word like ? order by population desc limit ?,?', [pattern, offset, limit]);
+  let words = await pool.query('select word,definition,us_audio,us_pron,has_audio from words where word like ? order by population desc limit ?,?', [pattern, offset, limit]);
   ctx.body = JSON.stringify(words);
   ctx.set('Access-Control-Allow-Origin', '*');
 });
