988c29785875e24f27cf7db46f83633ee3b41759
nestattacked <yang283349242@gmail.com>
2017-08-11 00:14:31 +0800

    complete style code && main.js

--- client/web/static/javascript/main.js
+++ client/web/static/javascript/main.js
@@ -1,4 +1,4 @@
-//helper
+//==================== util =====================//
 
 function debounce(idle, action) {
   var last;
@@ -11,20 +11,36 @@ function debounce(idle, action) {
   };
 };
 
-//render
+function decodeUnicode(str) {
+  return str.replace(/\\u[\dA-Fa-f]{4}/g, function(match){
+    return String.fromCharCode(parseInt(match.replace(/\\u/, ''), 16));
+  });
+}
+
+//==================== render =====================//
+
+function renderSearchResult(result){
+  var wordsHtml = JSON.parse(result).map(renderWord).join('');
+  if(wordsHtml === '')
+    wordsHtml = '<p class="no-result">抱歉，没有找到相关的内容</p>';
+  return wordsHtml;
+}
 
 function renderWord(word) {
   var playButton;
   if(word.has_audio) {
-    playButton = '<button class="result-play-button" data-url="'+word.us_audio+'">+</button>';
+    playButton = '<button class="result-play-button" data-url="' + word.us_audio + '"><img src="./static/img/play.png"></button>';
   }
   else {
     playButton = '<span class="result-no-play">暂无发音</span>';
   }
-  return '<div class="result-item"><span class="result-word">'+word.word+'</span><span class="result-pron">'+JSON.parse('"'+word.us_pron+'"')+'</span>'+playButton+'</div>';
+  var wordLiteral = '<span class="result-word">' + word.word + '</span>';
+  var wordPron = '<span class="result-pron">/ ' + decodeUnicode(word.us_pron) + ' /</span>';
+  var wordDefinition = '<span class="result-definition">' + decodeUnicode(word.definition) + '</span>';
+  return '<div class="result-item">' + wordLiteral + wordPron + playButton + wordDefinition + '</div>';
 }
 
-//action
+//==================== action =====================//
 
 function play(url) {
   var audio = $('#audio');
@@ -34,12 +50,19 @@ function play(url) {
 
 function search(pattern) {
   $.get('https://api.nestattacked.com/regdict/v1/words?pattern=' + pattern, function(result){
-    var wordsHtml = JSON.parse(result).map(renderWord).join('');
+    var wordsHtml = renderSearchResult(result);
     $('#results').html(wordsHtml);
+    $('#examples').hide();
+    $('#results').show();
   });
 }
 
-//initialize
+function showHelp(){
+  $('#results').hide();
+  $('#examples').show();
+}
+
+//==================== initilize =====================//
 
 var debouncedSearch = debounce(200, search);
 
@@ -50,3 +73,5 @@ $('#search-input').keyup(function(){
 $('#results').on('click', '.result-play-button', function(){
   play($(this).attr('data-url'));
 });
+
+$('#help-button').click(showHelp);
